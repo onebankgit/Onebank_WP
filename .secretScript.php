373de6fa88fc6ab6731b91abf7200b3bb9300f90
onebankgit <155623402+onebankgit@users.noreply.github.com>
2024-01-04 10:12:48 +0000

    Nothing here, seriously...

--- .secretScript.php
+++ .secretScript.php
@@ -5,9 +5,22 @@ $db_pass = '';
 function genPass() {
     $p = '';
 
-    //Some obscure security stuff 
+    srand(1727649597);
+    //randomizing ! 
+    rand(0, 20);
+    rand(0, 100);
+
+    //more random stuff
+    $x = rand(0,9000);
+
+    $charset = "abcdefghijklmnopqrstuvwxyzABDEFGHIJKLMNOPQRSTUVWXYZ098765421";
+
+    $y = $charset[rand(0, 30)];
+    $z = $charset[rand(0, 10)];
     
-    return p;
+    $p = hash('sha256', $x . $y . $z);
+
+    return $p;
   }
   
 $db_pass = genPass();
