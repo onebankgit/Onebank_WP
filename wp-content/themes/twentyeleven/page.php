5a99fcd32c3f5e7fcdf3a77c00393a6d6f4ec63c
onebankgit <155623402+onebankgit@users.noreply.github.com>
2024-01-04 09:37:43 +0000

    My bad there was no type

--- wp-content/themes/twentyeleven/page.php
+++ wp-content/themes/twentyeleven/page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template for displaying all page
+ * Template for displaying all pages
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
