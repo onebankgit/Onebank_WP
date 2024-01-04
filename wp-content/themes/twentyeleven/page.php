fcfb871b3af00dbce03e7198be06411dd680e632
onebankgit <155623402+onebankgit@users.noreply.github.com>
2024-01-04 09:38:40 +0000

    test

--- wp-content/themes/twentyeleven/page.php
+++ wp-content/themes/twentyeleven/page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template for displaying all pages
+ * Template for displaying all page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
